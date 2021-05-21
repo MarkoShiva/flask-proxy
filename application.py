5120431130f1411783a7ce18e5cbbb16ba729196
Marko Shiva Pavlovic <marko.shiva.pavlovic@gmail.com>
2021-05-21 17:41:01 +0200

    Its a bit hacky because of the https://www.mediawiki.org/wiki/ResourceLoader which refuse to get loaded deferred with flask. I could use uvsgi to do ajax but started with flask.

--- application.py
+++ application.py
@@ -1,51 +1,73 @@
-from flask import Flask,request,Response
+from flask import Flask, request ,Response
 import requests
 
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
+from werkzeug.serving import WSGIRequestHandler
 from replace_chars.main import replace_words
+import mimetypes
 
-app = Flask(__name__,  static_folder='static')
-LOCAL_NAME = 'https://localhost:8000'
-SITE_NAME = 'https://en.wikipedia.org'
+
+app = Flask(__name__, static_folder = "static/")
+mimetypes.add_type("text/javascript", ".js", False)
+mimetypes.add_type("text/css", ".css", False)
+LOCAL_NAME = '127.0.0.1'
+SITE_NAME = 'https://en.wikipedia.org/'
+
+
+
 
 
 @app.route('/')
 def index():
     return 'root'
 
-
-
-
-
-
-
-
 @app.route('/<path:path>', methods=['GET', 'POST', "DELETE"])
 def proxy(path):
     global SITE_NAME
     if request.method == 'GET':
-        resp = requests.get(f'{SITE_NAME}/{path}')
+        resp = requests.get(f'{SITE_NAME}{path}')
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        if 'php' not in path:
-            soup = bs(resp.content)
-            # with open("original.html", "w", encoding = 'utf-8') as file:
-            #     file.write(str(soup))
-            updated = replace_words(soup)
-            # with open("updated.html", "w", encoding = 'utf-8') as file:
-            #     file.write(str(updated))
-            resp._content = updated.encode('utf8')
-        response = Response(resp.content, resp.status_code, headers)
+        if 'php' not in path:    # exclude stuff that are not regular page request from soup
+            # append links or just download them with same filename like in page.
+            soup = BeautifulSoup(resp.content)
+            soup_append = BeautifulSoup('<link href="/static/css/Vector.css" rel="stylesheet" type="text/css"/>',
+                                        'html.parser')
+            soup.head.append(soup_append)
+            soup_append = BeautifulSoup('<script async="" src="/static/js/load.js"></script>', 'html.parser')
+            soup.head.append(soup_append)
+            soup_append = BeautifulSoup('<link href="/static/css/load.css" rel="stylesheet" type="text/css"/>',
+                                        'html.parser')
+            soup.head.append(soup_append)
+            soup_append = BeautifulSoup('<link href="/static/css/load1.css" rel="stylesheet" type="text/css"/>',
+                                        'html.parser')
+            soup.head.append(soup_append)
+            updated = replace_words(soup)   # run word replacement
+            resp._content = updated.encode('utf8')  # use setter and encode as bytestream
+            response = Response(resp.content, resp.status_code, headers, mimetype='text/html')
+            # specify response mimetype='text/html'
+        elif 'scripts' in request.args.values():
+            # print(request.args)
+            # specify response mimetype='text/javascript'
+            response = Response(resp.content, resp.status_code, headers, mimetype='text/javascript')
+        elif 'styles' in request.args.values():
+            # resp._content
+            # specify response mimetype='text/css'
+            response = Response(resp.content, resp.status_code, headers, mimetype='text/css')
+        else:
+            # do not modify
+            response = Response(resp.content, resp.status_code, headers)
         return response
+
     elif request.method == 'POST':
+        # we are not posting anything to wikipedia because proxy do not have need to edit as well. :)
         resp = requests.post(f'{SITE_NAME}/{path}',json=request.get_json())
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
         print(resp.content)
         response = Response(resp.content, resp.status_code, headers)
         return response
-
 
 
 
@@ -65,5 +87,6 @@ def proxy(path):
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", debug=True, port=8000)
-    # gunicorn.app(host="127.0.0.1", debug=True, port=8000)
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+    app.run(host=LOCAL_NAME, port=8000, debug=True)
+    # app.run(host=LOCAL_NAME, port=8000, debug=True)
--- replace_chars/main.py
+++ replace_chars/main.py
@@ -3,28 +3,25 @@ import re
 from bs4 import Tag, NavigableString, BeautifulSoup
 def replace_words(soup):
     phrase = "WARP!"
-    exp = re.compile(r"\b\w{5}\b")
-    if soup.find('body'):
-        page = soup.body.find_all(text = exp)
-        for i, line in enumerate(page):
-            if not re.match(r"^(?![http+|href])[[a-zA-Z0-9'\"]+", line):
+    exp = re.compile(r"\b\w{5}\b") # we are replacing any kind of word that is 5 char long. that include numbers.
+    if soup.find('body'):       # limit to body
+        page = soup.body.find_all(text = exp) # find all text that matches word len.
+        for line in page:
+            if not re.match(r"^(?!http+)[[a-zA-Z0-9'\"]+", line):
+                # refuse renaming links in text and make sure we are not renaming links
                 continue
-            elif not isinstance(line, NavigableString):
-                continue
-            words = re.findall(exp, line)
+            elif isinstance(line, NavigableString):
+                # only rename strings that are in text not sometime inlined script tags.
+                # words = re.findall(exp, line)  # this was debugging
 
 
-
-            print("="*25)
-            print(line)
-            print()
-            print(words)
-            print()
-            words = re.sub(exp, phrase, line)
-            # for word in words:
-                # replaced_word = line.replace(word, "WARP!")
-            # line.replace_with(replaced_word)
-            line.replace_with(words)
+                # we are doing a faster replace with re.
+                words = re.sub(exp, phrase, line)
+                # can be used in case of needed to do nested check replace
+                # for word in words:
+                    # replaced_word = line.replace(word, "WARP!")
+                # line.replace_with(replaced_word)
+                line.replace_with(words)
         print(soup.text.count(phrase))
     return soup
 
